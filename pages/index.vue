d00602d4294211a28a5f935912ae25a7203bdd30
hendrikbl <hendrik.bloess@googlemail.com>
2021-02-17 10:02:29 +0100

    added links to footer

--- pages/index.vue
+++ pages/index.vue
@@ -54,10 +54,13 @@
         <input
           id="playercount"
           v-model="playercount"
-          class="font-extrabold text-gray-800 focus:outline-none flex-none border-b-2 bg-transparent border-gray-500 p-2 mx-2"
+          :class="`font-extrabold text-gray-800 focus:outline-none flex-none border-b-2 bg-transparent border-${
+            playercount <= 20 ? 'gray' : 'red'
+          }-500 p-2 mx-2`"
           type="number"
           name="playercount"
           placeholder="Players"
+          min="0"
         />
       </div>
       <div v-for="player in players" :key="player.id" class="mx-5">
@@ -99,11 +102,26 @@
     </div>
     <footer class="lg:w-3/4 xl:w-2/3 mx-auto mt-10">
       <hr class="border-t-4 rounded-full my-5 mx-5" />
-      <div class="flex flex-row justify-center pb-5">
-        <div class="font-bold">
+      <div class="grid grid-cols-3 grid pb-5 mx-5">
+        <div class="text-left">
+          <a href="https://github.com/hendrikbl/mycafe-rewards-calculator">
+            <svg viewBox="0 0 24 24" class="h-6">
+              <path
+                fill="currentColor"
+                d="M12,2A10,10 0 0,0 2,12C2,16.42 4.87,20.17 8.84,21.5C9.34,21.58 9.5,21.27 9.5,21C9.5,20.77 9.5,20.14 9.5,19.31C6.73,19.91 6.14,17.97 6.14,17.97C5.68,16.81 5.03,16.5 5.03,16.5C4.12,15.88 5.1,15.9 5.1,15.9C6.1,15.97 6.63,16.93 6.63,16.93C7.5,18.45 8.97,18 9.54,17.76C9.63,17.11 9.89,16.67 10.17,16.42C7.95,16.17 5.62,15.31 5.62,11.5C5.62,10.39 6,9.5 6.65,8.79C6.55,8.54 6.2,7.5 6.75,6.15C6.75,6.15 7.59,5.88 9.5,7.17C10.29,6.95 11.15,6.84 12,6.84C12.85,6.84 13.71,6.95 14.5,7.17C16.41,5.88 17.25,6.15 17.25,6.15C17.8,7.5 17.45,8.54 17.35,8.79C18,9.5 18.38,10.39 18.38,11.5C18.38,15.32 16.04,16.16 13.81,16.41C14.17,16.72 14.5,17.33 14.5,18.26C14.5,19.6 14.5,20.68 14.5,21C14.5,21.27 14.66,21.59 15.17,21.5C19.14,20.16 22,16.42 22,12A10,10 0 0,0 12,2Z"
+              />
+            </svg>
+          </a>
+        </div>
+        <div class="font-bold text-center">
           Made with <span class="text-red-500">❤</span> by
           <a href="https://github.com/hendrikbl" class="underline">hendrikbl</a>
         </div>
+        <a
+          href="http://melsoft-games.com/"
+          class="font-semibold underline text-right"
+          >Melsoft Games</a
+        >
       </div>
     </footer>
   </div>
@@ -188,6 +206,8 @@ export default {
 
     calculateAll() {
       this.players.forEach((player) => {
+        if (this.totals.trophies === 0) return
+
         player.diamonds =
           this.totals.diamonds === 0 || player.id > this.playercount
             ? 0
