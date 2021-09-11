509ea4ec182607cc493011baa43044d9cd3978c1
hendrikbl <hendrikbl@users.noreply.github.com>
2021-09-11 14:47:18 +0000

    deploy: 66b39c2193919a3d467d911eed38b4e586667e62

--- _nuxt/1f15ca4.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[8],{743:function(e){e.exports=JSON.parse('{"Diamonds":"Diamantes","Name":"Nombre","Rubies":"Rubíes","Treasure_Trove":"Tesoro","Trophies":"Copas","meta:description":"Calculadora de recompensas para festivales MyCafé","Participants":"Participantes","Privacy":"Intimidad"}')}}]);
